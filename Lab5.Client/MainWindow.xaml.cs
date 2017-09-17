dddfe34c2b23006ba3ecaaddecfee84b14d97a93
Евгений Комиссаров <komissarovea@gmail.com>
2017-09-18 01:57:20 +0300

    Add Lab5 (hippodrome)

---
+++ Lab5.Client/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Lab5.HippodromeLib;
+using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Lab5.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructor
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            hippodromeControl1.PathBrush = Brushes.Green;
+            hippodromeControl1.HorseBrush = Brushes.Blue;
+            hippodromeControl2.PathRadius -= 30;
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            Random r = new Random();
+            hippodromeControl1.AngleSpeed = r.Next(1, 10) * 0.01;
+            hippodromeControl2.AngleSpeed = r.Next(1, 10) * 0.01;
+            hippodromeControl1.Start();
+            hippodromeControl2.Start();
+
+            btnStart.IsEnabled = false;
+            btnStop.IsEnabled = true;
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            hippodromeControl1.Stop();
+            hippodromeControl2.Stop();
+            btnStart.IsEnabled = true;
+            btnStop.IsEnabled = false;
+        }
+
+        private void horseLeftClick(object sender, EventArgs e)
+        {
+            HippodromeControl horseControl = sender as HippodromeControl;
+            MessageBox.Show("Horse Speed: " + horseControl.AngleSpeed + " rad/s",
+                "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void horseRightClick(object sender, EventArgs e)
+        {
+            HippodromeControl horseControl = sender as HippodromeControl;
+            MessageBox.Show(String.Format("Horse Coordinates: ({0:F0},{1:F0})", horseControl.CurrentPoint.X, horseControl.CurrentPoint.Y),
+               "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+        } 
+
+        #endregion
+    }
+}
---
+++ Lab5.HippodromeLib/HippodromeControl.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace Lab5.HippodromeLib
+{
+    /// <summary>
+    /// Interaction logic for HippodromeControl.xaml
+    /// </summary>
+    public partial class HippodromeControl : UserControl
+    {
+        #region Fields
+
+        private double horseRadius = 10;
+        private double angleSpeed = 0.01;
+        private double pathRadius = 200;
+        private Point currentPoint;
+        private int time = 0;
+        private DispatcherTimer dt = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 0, 0, 1) };
+
+        private Brush pathBrush;
+        private Brush horseBrush;
+
+        public event EventHandler HorseLeftClick;
+        public event EventHandler HorseRightClick;
+
+        #endregion
+
+        #region Properties
+
+        public Brush PathBrush
+        {
+            get { return pathBrush; }
+            set
+            {
+                pathBrush = value;
+                pathEllipse.Stroke = pathBrush;
+            }
+        }
+
+        public Brush HorseBrush
+        {
+            get { return horseBrush; }
+            set
+            {
+                horseBrush = value;
+                horseEllipse.Fill = value;
+            }
+        }
+
+        public double PathRadius
+        {
+            get
+            {
+                return pathRadius;
+            }
+            set
+            {
+                pathRadius = value;
+                UpdateRadius();
+            }
+        }
+
+        public double AngleSpeed
+        {
+            get { return angleSpeed; }
+            set { angleSpeed = value; }
+        }
+
+        public Point CurrentPoint { get { return currentPoint; } }
+
+        #endregion
+
+        #region Constructor
+
+        public HippodromeControl()
+        {
+            InitializeComponent();
+            dt.Tick += DtOnTick;
+            horseEllipse.Width = horseEllipse.Height = horseRadius * 2;
+            UpdateRadius();
+            //horseEllipse.Margin = new Thickness(pathRadius * 2 - horseRadius,
+            //    pathRadius - horseRadius, 0, 0);
+            //horseEllipse.Margin = new Thickness(currentPoint.X - horseRadius, currentPoint.Y - horseRadius, 0, 0);
+        } 
+        
+        #endregion
+
+        #region Methods
+
+        public void Start()
+        {
+            dt.Start();
+            currentPoint = new Point(pathRadius, 0);
+            horseEllipse.Margin = new Thickness(pathRadius + currentPoint.X - horseRadius,
+                                                pathRadius + currentPoint.Y - horseRadius, 0, 0);
+            horseEllipse.Visibility = Visibility.Visible;
+        }
+
+        public void Stop()
+        {
+            dt.Stop();
+            //horseEllipse.Visibility = Visibility.Collapsed;
+            time = 0;
+        }
+
+        private void UpdateRadius()
+        {
+            canvas.Width = canvas.Height = pathRadius * 2;
+            pathEllipse.Width = pathEllipse.Height = pathRadius * 2 + pathEllipse.StrokeThickness;
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        private void DtOnTick(object sender, EventArgs eventArgs)
+        {
+            time++;
+            double x = pathRadius * Math.Cos(AngleSpeed * time);
+            double y = pathRadius * Math.Sin(AngleSpeed * time);
+            currentPoint = new Point(x, y);
+            horseEllipse.Margin = new Thickness(pathRadius + currentPoint.X - horseRadius,
+                                               pathRadius + currentPoint.Y - horseRadius, 0, 0);
+            //Point newPoint;
+            //if (currentPoint.X > 0 || currentPoint.Y == -pathRadius)
+            //{
+            //    newPoint = new Point(currentPoint.X, currentPoint.Y + 1);
+            //    newPoint.X = Math.Sqrt(Math.Pow(pathRadius, 2) - Math.Pow(newPoint.Y, 2));
+
+            //}
+            //else
+            //{
+            //    newPoint = new Point(currentPoint.X, currentPoint.Y - 1);
+            //    newPoint.X = -Math.Sqrt(Math.Pow(pathRadius, 2) - Math.Pow(newPoint.Y, 2));
+            //}
+            //horseEllipse.Margin = new Thickness(pathRadius + newPoint.X - horseRadius,
+            //                                    pathRadius + newPoint.Y - horseRadius, 0, 0);
+            //currentPoint = newPoint;
+        }
+
+        private void horseEllipse_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            HorseLeftClick?.Invoke(this, new EventArgs());
+        }
+
+        private void horseEllipse_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            HorseRightClick?.Invoke(this, new EventArgs());
+        } 
+
+        #endregion
+    }
+}
