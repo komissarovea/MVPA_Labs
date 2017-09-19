e6a22882db1d63029e5ba9ef6820a24c3672b75d
Евгений Комиссаров <komissarovea@gmail.com>
2017-09-19 12:39:02 +0300

    Update lab 6.

--- Lab6.Integral/InputWindow.xaml.cs
+++ Lab6.Integral/InputWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace Lab6.Integral
 {
@@ -7,9 +8,29 @@ namespace Lab6.Integral
     /// </summary>
     public partial class InputWindow : Window
     {
+        public double LowLimit { get; set; }
+
+        public double UpperLimit { get; set; }
+
+        public int RegionSplitNumber { get; set; }
+
         public InputWindow()
         {
             InitializeComponent();
+            this.LowLimit = 0;
+            this.UpperLimit = Math.PI * 2;
+            this.RegionSplitNumber = 100000000;
+            this.DataContext = this;
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
         }
     }
 }
--- Lab6.Integral/MainWindow.xaml.cs
+++ Lab6.Integral/MainWindow.xaml.cs
@@ -10,19 +10,42 @@ namespace Lab6.Integral
     /// </summary>
     public partial class MainWindow : Window, IProgress<int>
     {
+        public double LowLimit { get; set; }
+
+        public double UpperLimit { get; set; }
+
+        public int RegionSplitNumber { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            this.LowLimit = 0;
+            this.UpperLimit = Math.PI * 2;
+            this.RegionSplitNumber = 100000000;
+            txtSettings.Text = $"Нижний предел: {LowLimit}\nВерхний предел: {UpperLimit}\nЧисло разбиений области: {RegionSplitNumber}";
         }
 
-        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        private void btnSettings_Click(object sender, RoutedEventArgs e)
+        {
+            InputWindow inputWindow = new InputWindow() { Owner = this, WindowStartupLocation = WindowStartupLocation.CenterOwner };
+            bool? result = inputWindow.ShowDialog();
+            if (result.HasValue && result.Value)
+            {
+                this.LowLimit = inputWindow.LowLimit;
+                this.UpperLimit = inputWindow.UpperLimit;
+                this.RegionSplitNumber = inputWindow.RegionSplitNumber;
+                txtSettings.Text = $"Нижний предел: {LowLimit}\nВерхний предел: {UpperLimit}\nЧисло разбиений области: {RegionSplitNumber}";
+            }
+        }
+
+        private async void btnStart_Click(object sender, RoutedEventArgs e)
         {
             Cursor = Cursors.Wait;
-            btn2.IsEnabled = false;
-            double resIntegral = await NumMethods.IntegralAsync(0, 1, 20000000, this);
-            labelRes.Content = resIntegral.ToString();
+            btnStart.IsEnabled = false;
+            double resIntegral = await NumMethods.IntegralAsync(this.LowLimit, this.UpperLimit, this.RegionSplitNumber, this);
+            lbResult.Content = $"Результат: {resIntegral}";
             Cursor = Cursors.Arrow;
-            btn2.IsEnabled = true;
+            btnStart.IsEnabled = true;
             progressBar1.Value = 100;
         }
 
@@ -35,35 +58,38 @@ namespace Lab6.Integral
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //int nCPU = Environment.ProcessorCount;
-            //Title = "Cpu=" + nCPU;
+            ////int nCPU = Environment.ProcessorCount;
+            ////Title = "Cpu=" + nCPU;
 
-            Cursor = Cursors.Wait;
-            btn.IsEnabled = false;
+            //Cursor = Cursors.Wait;
+            //btn.IsEnabled = false;
 
-            //double integralValue1 = NumMethods.Integral(0, 1, 100000000);
-            //labelRes.Content = integralValue1.ToString();
+            ////double integralValue1 = NumMethods.Integral(0, 1, 100000000);
+            ////labelRes.Content = integralValue1.ToString();
 
-            Task<double> task = new Task<double>(() =>
-            {
-                double integralValue = NumMethods.Integral(0, 1, 100000000);
-                return integralValue;
-            });
-            task.Start();
+            //Task<double> task = new Task<double>(() =>
+            //{
+            //    double integralValue = NumMethods.Integral(0, 1, 100000000);
+            //    return integralValue;
+            //});
+            //task.Start();
 
-            task.ContinueWith((task2) =>
-            {
-                double integralValue = task2.Result;
-                //labelRes.Content = integralValue.ToString();
-                Action action = () =>
-                {
-                    labelRes.Content = integralValue.ToString();
-                    Cursor = Cursors.Arrow;
-                    btn.IsEnabled = true;
-                };
-                Dispatcher.Invoke(action);
-            });
-        } 
+            //task.ContinueWith((task2) =>
+            //{
+            //    double integralValue = task2.Result;
+            //    //labelRes.Content = integralValue.ToString();
+            //    Action action = () =>
+            //    {
+            //        labelRes.Content = integralValue.ToString();
+            //        Cursor = Cursors.Arrow;
+            //        btn.IsEnabled = true;
+            //    };
+            //    Dispatcher.Invoke(action);
+            //});
+        }
+
         #endregion
+
+
     }
 }
